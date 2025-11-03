100817f209ec445dd167db6b53934497f0e335a4
xiaoyuanyuan-rush <2497822530@qq.com>
2025-11-04 02:05:02 +0800

    feat: 新增打包mock

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
       vue(),
       viteMockServe({
         mockPath: 'mock',
-        enable: command === 'serve',
+        enable: command === 'serve' || command === 'build', // 修改这里，让build时也启用
         logger: true,
         supportTs: false
       })
